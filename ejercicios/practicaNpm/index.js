7f2f5b985633dd22c26233d871dc7da56f6b768c
Diego Barletta <diegohbarletta@gmail.com>
2025-05-10 21:22:54 -0300

    limpieza archivo index

--- ejercicios/practicaNpm/index.js
+++ ejercicios/practicaNpm/index.js
@@ -23,43 +23,11 @@ console.log(div(30, 3));
 console.log(argu[1]);
 
 const args = process.argv.slice(2); // Ignoramos los dos primeros elementos con slice 
-/*if (args[0] === 'saludar') { 
+if (args[0] === 'saludar') { 
     console.log(`¡Hola, ${args[1] || 'mundo'}!`); 
 } else if (args[0] === 'despedir') { 
     console.log(`¡Adiós, ${args[1] || 'mundo'}!`); 
 } else { 
     console.log('Comando no reconocido. Usa "saludar" o "despedir".');
-}*/
-
-switch (args[0]) {
-    case 'GET':
-        if (args[1] != undefined) {
-            console.log(`Toma un dato`);
-        } else {
-            console.log("No se ha pasado un dato");
-        }
-        break;
-    case 'POST':
-        if (args[1] === undefined) {
-            console.log(`No se ha pasado el id`);
-        } else {
-            console.log(`Recibimos ${args[1]}  satisfactoriamente`);
-        }
-        break;
-    case 'PUT':
-        if (args[1] != undefined) {
-            console.log(`Modificamos el item con id: ${args[1]}  satisfactoriamente`);
-        } else {
-            console.log(`No se ha podido modificar el item, no se ha pasado el id`);
-        }
-        break;
-    case 'DELETE':
-        if (args[1] !== undefined) {
-            console.log(`El item con id: ${args[1]}  se eliminó con éxito`);
-        } else {
-            console.log(`No se ha podido eliminar el item, no se ha pasado el id`);
-        }
-        break;
-    default:
-        console.log('Comando no reconocido.');
 }
+
